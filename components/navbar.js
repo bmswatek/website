0481d5634272421e77e3fb7cf46a7a1e14c36e1d
Bartek Swatek <bmswatek@gmail.com>
2025-08-04 19:23:36 +0100

    Navbar adjustment

--- components/navbar.js
+++ components/navbar.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="pt-safe p-4">
-      <div className="flex justify-center space-x-6 text-[#00ffcc] text-lg">
+      <div className="flex flex-wrap justify-center gap-4 text-[#00ffcc] text-lg">
         <Link href="/" className="hover:text-green-400 transition">{'{ home }'}</Link>
         <Link href="/about" className="hover:text-green-400 transition">{'{ about }'}</Link>
         <Link href="/projects" className="hover:text-green-400 transition">{'{ projects }'}</Link>
